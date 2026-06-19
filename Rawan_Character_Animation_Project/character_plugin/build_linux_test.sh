45bbf4498d613f7f65db373bf6599d5b790b5563
rawanakrum <rawan.akrum@gmail.com>
2026-06-19 10:06:03 +0300

    Delete Rawan_Character_Animation_Project directory

--- Rawan_Character_Animation_Project/character_plugin/build_linux_test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-mkdir -p build
-g++ -std=c++17 -I include tests/standalone_test.cpp src/StudentController.cpp -o build/standalone_test
-./build/standalone_test
